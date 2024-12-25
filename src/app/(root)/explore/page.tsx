608e1630bc677c6f7820d8e2cb8c06f90636bed8
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2024-12-25 15:06:06 +0500

    started explore page

--- src/app/(root)/explore/page.tsx
+++ src/app/(root)/explore/page.tsx
@@ -1,9 +1,43 @@
+import Flicks from '@/components/Flicks';
+import HashtagBox from '@/components/HashtagBox';
+import Heading from '@/components/Heading'
+import PostsGrid from '@/components/PostsGrid';
+import QueryForm from '@/components/QueryForm'
 import React from 'react'
 
-const ExplorePage = () => {
-  return (
-    <div>ExplorePage</div>
-  )
+const ExplorePage = async ({ searchParams }: { searchParams: { hashtag_query: string } }) => {
+
+  const usableParams = await searchParams;
+  
+  let posts;
+
+   posts = await getPopularTodayPosts() 
+
+
+    return (
+        <main className=''>
+            <section>
+                <Heading icon='/icons/flicks-white.svg' text='Search Hashtags' className={'justify-center'} />
+
+                <QueryForm
+                    name={'hashtag_query'}
+                    query={usableParams.hashtag_query}
+                    action='/flicks'
+                    icon='/icons/search.svg' />
+
+                <div className='flex justify-center gap-4'>
+                    <HashtagBox text='webdevelopment' />
+                    <HashtagBox text='mountains' />
+                    <HashtagBox text='sunrise' />
+                    <HashtagBox text='funny' />
+                </div>
+            </section>
+
+            <section className='pt-10 flex flex-wrap gap-8'>
+          <PostsGrid posts={posts} />
+            </section>
+        </main>
+    )
 }
 
 export default ExplorePage
