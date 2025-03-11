06bb2d3356da0becea61d6cc19d49f4b5c16d478
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2025-03-11 16:08:39 +0500

    remove messages inclusion from getChatById action

--- src/actions/chat.actions.ts
+++ src/actions/chat.actions.ts
@@ -93,7 +93,6 @@ export const getChatById = async (chatId: string) => {
                 id: chatId
             },
             include: {
-                messages: true,
                 users: true
             }
         });
