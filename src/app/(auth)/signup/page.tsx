a20a6d32795875cb0d3ae39a7c7c9d1500d27757
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2025-03-24 15:31:07 +0500

    refactor: update SignUpPage to use PageProps for improved type handling

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,22 +1,23 @@
 import AuthForm from "@/components/forms/AuthForm"
 import { toast } from "@/hooks/use-toast";
 import { Metadata } from "next";
+import { PageProps } from "../../../../.next/types/app/(auth)/signup/page";
 
 export const metadata: Metadata = {
   title: 'Signup',
   description: 'Signup to create your account',
 }
 
-export default async function SignUpPage({ searchParams }: { searchParams: { callbackUrl: string, error: string } }) {
+export default async function SignUpPage({ searchParams }: PageProps) {
 
-const usableParams = await (searchParams);
+  const usableParams = await (searchParams);
 
-if (usableParams.error === 'OAuthAccountNotLinked') {
-  toast({
-    variant: 'destructive',
-    title: 'The email you are trying to use is already linked to another account. Try logging in through that authentication provider'
-  })
-}
+  if (usableParams.error === 'OAuthAccountNotLinked') {
+    toast({
+      variant: 'destructive',
+      title: 'The email you are trying to use is already linked to another account. Try logging in through that authentication provider'
+    })
+  }
 
   return (
     <AuthForm
